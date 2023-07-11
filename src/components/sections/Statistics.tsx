a3bd44f2d82abc1b99e858511814f1c9de46c38d
Ruy Monteiro <ruystfrancis@gmail.com>
2023-07-11 10:56:09 -0300

    fix(stats): Align items

--- src/components/sections/Statistics.tsx
+++ src/components/sections/Statistics.tsx
@@ -8,7 +8,7 @@ interface StatisticsSectionProps {};
 export function StatisticsSection({}: StatisticsSectionProps) {
   return (
     <section id="statistics" className="wrapper lg:-mt-10">
-      <div className="md:place-center grid grid-cols-1 rounded-xl px-6 py-8 sm:grid-cols-2 sm:border sm:border-amber-700/10 sm:bg-amber-700/[2%] md:grid-cols-4 md:place-content-between">
+      <div className="md:place-center grid grid-cols-1 rounded-xl px-6 py-8 sm:grid-cols-2 sm:border sm:border-amber-700/10 sm:bg-amber-700/[2%] md:grid-cols-3 md:place-content-between">
         {statistics.map((item, index) => (
           <StatisticsItem key={index} {...item} />
         ))}
