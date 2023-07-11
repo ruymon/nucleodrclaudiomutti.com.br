da8fe8c093f38d8201a97856a9b843be8de59231
Ruy Monteiro <ruystfrancis@gmail.com>
2023-07-11 12:46:03 -0300

    revert(font)

--- src/components/primitives/Heading.tsx
+++ src/components/primitives/Heading.tsx
@@ -8,7 +8,7 @@ const headingVariants = cva(
     variants: {
       variant: {
         h1: "text-5xl lg:text-6xl",
-        h2: "text-4xl lg:text-5xl",
+        h2: "text-3xl lg:text-4xl",
         h3: "text-2xl",
         span: "",
       },
