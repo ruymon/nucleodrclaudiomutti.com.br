94d8dd486682b10e5b20fb4fcd212b73bf7e265d
Ruy Monteiro <ruystfrancis@gmail.com>
2023-07-11 12:27:26 -0300

    fix(doctor): Remove institutions

--- src/helpers/doctors.tsx
+++ src/helpers/doctors.tsx
@@ -6,7 +6,7 @@ export const doctors: IDoctor[] = [
     name: "Dr. Claudio Mutti",
     registration: "CRM 59687/SP",
     specialty: ["Nutrologia"],
-    description: "Olá! Sou o Dr. Claudio Mutti, médico formado pela UNIVAS com mais de três décadas de experiência na área da saúde. Após meus vários anos de atendimentos, decidi fundar o meu próprio núcleo, com um único objetivo: Ajudar você a se sentir bem, com longevidade e bem-estar estético. Para isso, desenvolvi uma metodologia personalizada e centrada no paciente para potencializar seus resultados, atendendo sempre com excelência! Por fim, não deixe de me acompanhar nas minhas redes sociais! Diariamente compartilho dicas para te ajudar a harmonizar uma alimentação saudável e a adoção de bons hábitos.",
+    description: "Olá! Sou o Dr. Claudio Mutti, médico, com mais de três décadas de experiência na área da saúde. Após meus vários anos de atendimentos, decidi fundar o meu próprio núcleo, com um único objetivo: Ajudar você a se sentir bem, com longevidade e bem-estar estético. Para isso, desenvolvi uma metodologia personalizada e centrada no paciente para potencializar seus resultados, atendendo sempre com excelência! Por fim, não deixe de me acompanhar nas minhas redes sociais! Diariamente compartilho dicas para te ajudar a harmonizar uma alimentação saudável e a adoção de bons hábitos.",
     image: "/images/doctors/doctor-claudio.jpeg",
     social: [
       {
@@ -27,7 +27,7 @@ export const doctors: IDoctor[] = [
     name: "Dr. Marcelo Lima",
     registration: "CRM 206295/SP",
     specialty: ["Obesidade", "Sarcopenia"],
-    description: "Olá! Sou o Dr. Marcelo Lima, médico formado pela Universidade Nove de Julho, com anos de experiência em diversas áreas da medicina. Meu foco é ajudar meus pacientes a alcançarem uma vida saudável e equilibrada. Como especialista em emagrecimento e longevidade, busquei aprimorar meus conhecimentos nas áreas de bem-estar e medicina integrativa, esportiva, modulação hormonal e hipertrofia. Estou sempre atualizado com as tendências mais modernas da medicina para oferecer os melhores cuidados aos meus pacientes. Além do meu trabalho clínico, compartilho regularmente dicas e orientações valiosas em minhas redes sociais, fique por dentro.",
+    description: "Olá! Sou o Dr. Marcelo Lima, médico, com anos de experiência em diversas áreas da medicina. Meu foco é ajudar meus pacientes a alcançarem uma vida saudável e equilibrada. Como especialista em emagrecimento e longevidade, busquei aprimorar meus conhecimentos nas áreas de bem-estar e medicina integrativa, esportiva, modulação hormonal e hipertrofia. Estou sempre me atualizando com as práticas mais modernas da medicina para oferecer os melhores cuidados aos meus pacientes. Além do meu trabalho clínico, compartilho regularmente dicas e orientações valiosas em minhas redes sociais, fique por dentro.",
     image: "/images/doctors/doctor-marcelo.jpeg",
     social: [
       {
